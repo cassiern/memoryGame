9741d3c2ed4431a7da2640d964b534ad826d4aea
Cassie Nichols <cassienichols@macbooks-MacBook-Pro.local>
2019-06-16 19:10:08 -0600

    Created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -33,19 +33,28 @@ function checkForMatch() {
 }
 
 function flipCard(cardId) {
-
-	var flippedCard = cards[cardId];
-
+	var cardId = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardId].rank);
-
-	console.log("User flipped " +  cards[cardId].rank + ".");
-	console.log(cards[cardId].cardImage);
-
+	
+	this.setAttribute('src', cards[cardId].cardImage);
 	if (cardsInPlay.length === 2) {
 		checkForMatch()
 	};
+	console.log("User flipped " +  cards[cardId].rank + " of " + cards[cardId].suit + ".");
+	console.log(cards.cardImage);
+}
+	
+
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
 }
 
+createBoard()
 
-flipCard(0);
-flipCard(1);
+
